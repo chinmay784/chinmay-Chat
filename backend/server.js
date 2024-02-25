d794ee526ef3488e8b1418ce4950968ce7ab3034
Chinmay Puhan <122338728+chinmay784@users.noreply.github.com>
2024-02-25 11:15:18 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,15 @@ const port = process.env.PORT || 4000;
 app.use(express.json());
 app.use(cookieparser());
 
+app.use(cors(
+    {
+     origin:["https://chinmay-chat-x5sk.vercel.app/"] ,
+     methods:["POST","GET"],
+     credentials:true,
+
+    }
+));
+
 app.get("/",(req,res) =>{
     res.json("Hello")
 })
