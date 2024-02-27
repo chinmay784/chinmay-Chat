a40b5fde26d619d46ec51069728167dd98fc2741
Chinmay Puhan <122338728+chinmay784@users.noreply.github.com>
2024-02-27 08:29:26 +0530

    Update useGetConversation.js

--- frontend/src/hooks/useGetConversation.js
+++ frontend/src/hooks/useGetConversation.js
@@ -9,7 +9,7 @@ const useGetConversation = () => {
     const getConversation = async () =>{
         setLoading(true)
         try {
-            const res = await fetch("/api/users");
+            const res = await fetch("/users");
             const data = await res.json();
             // console.log(data)
 
