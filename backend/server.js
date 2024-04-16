2190235b338f617ed799a07b5db50063093acfda
Chinmay Puhan <122338728+chinmay784@users.noreply.github.com>
2024-04-16 13:55:34 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -34,15 +34,15 @@ app.use("/api/auth",authRoute);
 app.use("/api/messages",messageRoute);
 app.use("/api/users",userroute);
 
-const __dirname1 = path.resolve()
+// const __dirname1 = path.resolve()
 
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname1, '/frontend/dist')));
+// if(process.env.NODE_ENV === "production"){
+//     app.use(express.static(path.join(__dirname1, '/frontend/dist')));
 
-    app.get("*", (req, res) => {
-	  res.sendFile(path.resolve(__dirname1, 'frontend', 'dist', 'index.html'));
-   });
-}
+//     app.get("*", (req, res) => {
+// 	  res.sendFile(path.resolve(__dirname1, 'frontend', 'dist', 'index.html'));
+//    });
+// }
 
 server.listen(port,() =>{
     connectTOmongoDB();
