c830bf8b888a7f4c960a4a7ca4f5bff78920f0c7
Chinmay Puhan <122338728+chinmay784@users.noreply.github.com>
2024-04-22 19:38:27 +0530

    Update auth_route.js

--- backend/routes/auth_route.js
+++ backend/routes/auth_route.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const { signup, login, logout } = require("../controllers/auth_controller");
+const { protectRoute } = require("../middelweres/protectRoutes");
 
 const router = express.Router();
 
-router.post("/signup", signup);
-router.post("/login",login);
-router.post("/logout",logout);
+router.post("/signup",protectRoute, signup);
+router.post("/login",protectRoute,login);
+router.post("/logout",protectRoute,logout);
 
 
 module.exports = router
